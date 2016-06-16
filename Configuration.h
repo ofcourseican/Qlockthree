504bd8d126122062c421a16cdc08a2d309f10128
bracci <manuel.bracher@gmail.com>
2016-06-16 22:37:51 +0200

    Modifications for vertical LPD8806 RGBW strips

--- Configuration.h
+++ Configuration.h
@@ -108,8 +108,8 @@
  #define LED_DRIVER_LPD8806
 
 #if defined(LED_DRIVER_LPD8806) || defined(LED_DRIVER_NEOPIXEL)
-#define RGB_LEDS
-//#define RGBW_LEDS
+//#define RGB_LEDS
+#define RGBW_LEDS
 //#define MATRIX_XXL
 #endif
 
